ffa13b7827c49a69b00d449fe3d2c7963fc01457
Gowsik <gowsik@mallow-tech.com>
2020-12-04 13:07:30 +0530

    Added failing tests

--- spec/features/posts/post_spec.rb
+++ spec/features/posts/post_spec.rb
@@ -1,11 +1,29 @@
 require "rails_helper"
 
 RSpec.feature "post", js: true do
-  scenario "A post can be viewed" do
+  scenario "A post can be viewed(working spec 1)" do
     visit "/posts/new"
 
     fill_in "Title", :with => "My Widget"
     click_button "Create Post"
+
+    expect(page).to have_text("Post was successfully created.")
+  end
+  
+  scenario "A post can be viewed(failing spec)" do
+    visit "/posts/new"
+
+    fill_in "Title", :with => "My Widget"
+    click_button "Create Posts"
+
+    expect(page).to have_text("Post was successfully created.")
+  end
+
+  scenario "A post can be viewed(working spec 2)" do
+    visit "/posts/new"
+
+    fill_in "Title", :with => "My Widget"
+    click_button "Create Pos"
 
     expect(page).to have_text("Post was successfully created.")
   end
