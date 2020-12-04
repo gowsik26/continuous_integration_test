172abc62230633367fa731b264782611a2ee2454
Gowsik <gowsik@mallow-tech.com>
2020-12-04 13:39:36 +0530

    check bundle gem caching

--- spec/features/posts/post_spec.rb
+++ spec/features/posts/post_spec.rb
@@ -10,20 +10,11 @@ RSpec.feature "post", js: true do
     expect(page).to have_text("Post was successfully created.")
   end
   
-  scenario "A post can be viewed(failing spec)" do
+  scenario "A post can be viewed(passing spec)" do
     visit "/posts/new"
 
     fill_in "Title", :with => "My Widget"
     click_button "Create Post"
-
-    expect(page).to have_text("Post was successfully created.")
-  end
-
-  scenario "A post can be viewed(working spec 2)" do
-    visit "/posts/new"
-
-    fill_in "Title", :with => "My Widget"
-    click_button "Create Pos"
 
     expect(page).to have_text("Post was successfully created.")
   end
