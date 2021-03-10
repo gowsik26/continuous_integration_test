07bb56e61197db79ac4b2ac9576844c9ea5df65e
gowsik <gowsik@mallow-tech.com>
2021-03-10 11:08:28 +0530

    test [skip ci]

--- spec/features/posts/post3_spec.rb
+++ spec/features/posts/post3_spec.rb
@@ -19,6 +19,15 @@ RSpec.feature "posts3", js: true do
     expect(page).to have_text("Post was successfully created.")
   end
 
+  scenario "A post can be viewed(passing spec)" do
+    visit "/posts/new"
+
+    fill_in "Title", :with => "My Widget"
+    click_button "Create Post"
+
+    expect(page).to have_text("Post was successfully created.")
+  end
+
   scenario "A post can be viewed(passing spec 2)" do
     visit "/posts/new"
 
